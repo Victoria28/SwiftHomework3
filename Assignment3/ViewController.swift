c4ce363a1de784dfffabf5d3c3c0b1048768b970
vika mckee <vikaprus1993@gmail.com>
2018-07-17 00:12:32 +0300

    Got rid of capital letters and cleared up the code

--- Assignment3/ViewController.swift
+++ Assignment3/ViewController.swift
@@ -18,44 +18,33 @@ class ViewController: UIViewController {
         
         
         print("Method 1")
-        
-        
-        TheLargestOutOfTwoNumbers(number1: 6, number2: 20 )
+        theLargestOutOfTwoNumbers(number1: 6, number2: 20 )
         
         print("Method 2")
-        
-        print(NumberToThePowerOf2And3(number:5))
+        print(numberToThePowerOf2And3(number:5))
         
         print("Method 3")
-        
-        DisplayNumbersFrom0ToXAndInReverseOrder(numberx: 20)
+        displayNumbersFrom0ToXAndInReverseOrder(numberx: 20)
         
         print("Method 4")
-        
-        print(CountingDivisorsOfANumber(6))
+        print(countingDivisorsOfANumber(6))
         
         print("Method 5")
-        
-        print(FindingOutIfANUmberIsPerfect(7))
+        print(findingOutIfANUmberIsPerfect(7))
         
         print("-----------------Block 2---------------")
         
         print("Method 6")
-        
-        print(" The islanders would have",IslandPurchasedFor$24(24.0, 2018, 1826, interests: 6), "if they had put the money in a bank with 6% interest")
+        print(" The islanders would have",islandPurchasedFor$24(24.0, 2018, 1826, interests: 6), "if they had put the money in a bank with 6% interest")
         
         print("Method 7")
-        
-        print("A student needs", StudentTryingToSurviveOn1000Hrn(scholarship: 700, expenses: 1000),"on top of the yearly scholarship to survive a year")
+        print("A student needs", studentTryingToSurviveOn1000Hrn(scholarship: 700, expenses: 1000),"on top of the yearly scholarship to survive a year")
         
         print ("Method 8")
-        
-        
-        print("The money will last",TheAmountOfMonthsMoneyWillLast(scholarship: 700, expenses: 1000, savings: 2400), "months")
+        print("The money will last",theAmountOfMonthsMoneyWillLast(scholarship: 700, expenses: 1000, savings: 2400), "months")
         
         print(" Method 9")
-        
-        print("The flipped number is", ReverseOrderOfANumber(105))
+        print("The flipped number is", reverseOrderOfANumber(105))
         
         
         
@@ -64,24 +53,24 @@ class ViewController: UIViewController {
         
     }
     
-    func TheLargestOutOfTwoNumbers (number1:Int, number2:Int) {
+    func theLargestOutOfTwoNumbers(number1:Int, number2:Int) {
         
-        if number1>number2{
+        if number1>number2 {
             print("The larger number is", number1)
             
         }
-        else if number2>number1{
+        else if number2>number1 {
             print("The larger number is", number2)
             
         }
-        else{
+        else {
             print("number1 equals number2")
             
         }
         
     }
     
-    func NumberToThePowerOf2And3 (number: Int)->(result1:Int, result2: Int) {
+    func numberToThePowerOf2And3(number: Int) -> (result1:Int, result2: Int) {
         var result1: Int
         var result2: Int
         result1 = number * number
@@ -90,29 +79,29 @@ class ViewController: UIViewController {
         
     }
     
-    func DisplayNumbersFrom0ToXAndInReverseOrder(numberx: Int){
+    func displayNumbersFrom0ToXAndInReverseOrder(numberx: Int) {
         let number = numberx
         
         print("Original order:")
-        for i in 0..<number + 1{
+        for i in 0...number {
             print(i)
         }
         print("Reverse order:")
-        for i in 0..<number + 1{
+        for i in 0...number {
             print(number-i)
             
         }
     }
     
-    func CountingDivisorsOfANumber(_ number: Int)-> Int{
+    func countingDivisorsOfANumber(_ number: Int) -> Int {
         var result = 0
         let number = number
         var divisor = 0
         
-        while divisor < number{
+        while divisor < number {
             
             divisor = divisor + 1
-            if number % divisor == 0{
+            if number % divisor == 0 {
                 result += 1
                 
             }
@@ -120,7 +109,7 @@ class ViewController: UIViewController {
         return result
         
     }
-    func FindingOutIfANUmberIsPerfect (_ number: Int) -> Bool{
+    func findingOutIfANUmberIsPerfect(_ number: Int) -> Bool {
         let number = number
         var divisor = 0
         var sum = 0
@@ -134,25 +123,25 @@ class ViewController: UIViewController {
                 sum = sum + divisor
             }
         }
-        if sum == number{
+        if sum == number {
             return true
         }
         return false
     }
     
-    func IslandPurchasedFor$24 (_ InitialCost: Double, _ CurrentYear: Int, _ YearOfPurchase: Int, interests: Double)->Double{
+    func islandPurchasedFor$24(_ InitialCost: Double, _ CurrentYear: Int, _ YearOfPurchase: Int, interests: Double) -> Double {
         
         let timePassed = CurrentYear - YearOfPurchase
         var cost = InitialCost
         
-        for _ in 0..<timePassed{
+        for _ in 0..<timePassed {
             cost = cost * (interests/100 + 1)
             
         }
         return cost
     }
     
-    func StudentTryingToSurviveOn1000Hrn (scholarship: Double, expenses: Double) ->Double{
+    func studentTryingToSurviveOn1000Hrn(scholarship: Double, expenses: Double) -> Double {
         var expenses = expenses
         var necessaryAmountOfMoneyToSurvive = 0.0
         for _ in 0..<10 {
@@ -166,12 +155,12 @@ class ViewController: UIViewController {
         return necessaryAmountOfMoneyToSurvive
     }
     
-    func TheAmountOfMonthsMoneyWillLast (scholarship: Double, expenses: Double, savings: Double) -> Double{
+    func theAmountOfMonthsMoneyWillLast(scholarship: Double, expenses: Double, savings: Double) -> Double {
         var expenses = expenses
         var necessaryAmountOfMoneyToSurvive = 0.0
         var amountOfMonths = 0.0
         
-        for _ in 0..<10{
+        for _ in 0..<10 {
             expenses = expenses * 1.03
             necessaryAmountOfMoneyToSurvive = necessaryAmountOfMoneyToSurvive + expenses
         }
@@ -180,10 +169,10 @@ class ViewController: UIViewController {
     }
     
     
-    func ReverseOrderOfANumber (_ number: Int)-> Int{
+    func reverseOrderOfANumber(_ number: Int) -> Int {
         var result=0
         var number = number
-        while (number != 0){
+        while (number != 0) {
             result = result * 10 + number % 10
             number = number / 10
             
